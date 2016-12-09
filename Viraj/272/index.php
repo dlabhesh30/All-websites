209de9f86477c11fdfb5028343f83cd1569b65b3
sidharthbhasin <sidharth.bhasin@sjsu.edu>
2016-12-09 15:11:43 -0800

    adding chat window

--- Viraj/272/index.php
+++ Viraj/272/index.php
@@ -671,5 +671,20 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 }(document, 'script', 'facebook-jssdk'));</script>
 
 <script src="https://apis.google.com/js/platform.js" async defer></script>
+
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/584b0a8ee2def07b70ab53ce/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
+
 </body>
 </html>

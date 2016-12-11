b8c544fe182b85d8ce09f99035675d1c5ec055bc
virajnilakh <virajnilakh220@gmail.com>
2016-12-10 22:48:44 -0800

    top 5 rating

--- arun car/getOne.php
+++ arun car/getOne.php
@@ -11,7 +11,7 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
-$sql = "SELECT * FROM Products";
+$sql = "SELECT * FROM Products ORDER BY Rating DESC";
 
 $result = $conn->query($sql);
 if ($result->num_rows > 0) {

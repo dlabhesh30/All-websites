d3b28232f4b04732ad7d840f70086f23f64ac773
sidharthbhasin <sidharth.bhasin@sjsu.edu>
2016-12-10 22:35:06 -0800

    chat window
    ssjdn

--- Viraj/272/addtocart.php
+++ Viraj/272/addtocart.php
@@ -44,5 +44,18 @@ $conn->close();
 print("Added ".$Name." into ".$username." 's cart.");
 
 ?>
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/584b0a8ee2def07b70ab53ce/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
 </body>
 </html>
